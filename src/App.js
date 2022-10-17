823d227d314fc76fb2b9b81ba4ba2ff3f17486fd
James-Fraser1 <jdfraser2022@gmail.com>
2022-10-17 16:04:01 -0400

    portfolio copied over from broken repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+// import { Routes, Route } from 'react-router-dom';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
+
+// import About from './pages/About';
+// import Projects from './pages/Projects';
+// import Contact from './pages/Contact';
+
+// function App() {
+//   return (
+//     <div className='background'>
+//       <Header />
+//       <Routes>
+//         <Route path='/' element={<About />} />
+//         <Route path='/contact' element={<Contact />} />
+//         <Route path='/projects' element={<Projects />} />
+//       </Routes>
+//       <Footer></Footer>
+//     </div>
+
+//   );
+// }
+
+// export default App;
+
+import { useState } from "react";
+import Footer from "./components/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Main from './components/Main';
+import ReactDOM from 'react-dom'
+
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
 
 function App() {
+  const [page, setPage] = useState('about')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='background'>
+      <Header setPage={setPage} />
+      <Main page={page} />
+      <Footer />
     </div>
   );
 }
